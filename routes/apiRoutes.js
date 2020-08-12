36486153501f2fbf041d5b8c0680cff1c564666e
Ayodeji Amosun <ayodeji.amosun04@gmail.com>
2020-08-12 11:30:53 -0500

    started the api and html routes!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,43 @@
-const router = require("express").Router();
-const store = require("../db/store");
+// Dependencies
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid"); // allows for unique ID generation
 
-// GET "/api/notes" responds with all notes from the database
-router.get("/notes", (req, res) => {
-  store
-    .getNotes()
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
-});
+// Routes
+module.exports = function(app) {
+  // API GET Request
+  app.get("/api/notes", function(req, res) {
+    // console.log("get");    
+    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/db.json"), "utf8"));
+    res.send(notes);
+    // console.log(notes);
+  });
 
-router.post("/notes", (req, res) => {
-  store
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
-});
+  // API POST Request
+  app.post("/api/notes", function(req, res) {
+    // console.log("post");
+    let notesRead = JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/db.json"), "utf8"));
+    let newNote = req.body;
+    newNote.id = uuidv4();
+    // console.log(newNote.id);
+    notesRead.push(newNote);
+    let notesWrite = JSON.stringify(notesRead);
+    fs.writeFileSync(path.join(__dirname, "../../db/db.json"), notesWrite, "utf8");
+    // console.log(newNote);
+    // console.log(notesRead);
+    // console.log(notesWrite);
+    res.end();
+  });
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
-router.delete("/notes/:id", (req, res) => {
-  store
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
-});
-
-module.exports = router;
+  // API DELETE
+  app.delete("/api/notes/:id", function(req, res) {
+    // console.log("delete");
+    // console.log(req.params.id);
+    let notesRead = JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/db.json"), "utf8"));
+    let deleteId = req.params.id;
+    let updatedNotes = JSON.stringify(notesRead.filter(el => el.id !== deleteId));
+    fs.writeFileSync(path.join(__dirname, "../../db/db.json"), updatedNotes, "utf8");
+    // console.log(updatedNotes);
+    res.end();
+  });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,14 @@
-const path = require("path");
-const router = require("express").Router();
+// Dependencies
+var path = require("path");
 
-// "/notes" responds with the notes.html file
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-// All other routes respond with the index.html file
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-module.exports = router;
+// Routes
+module.exports = function(app) {
+  
+  // HTML GET Requests
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../notes.html"));
+  });
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+  });
+};

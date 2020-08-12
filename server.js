787a6ccfc423921b4696e9922bf7987c0dbe6983
Ayodeji Amosun <ayodeji.amosun04@gmail.com>
2020-08-12 11:31:29 -0500

    updated the server info!

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+// Dependencies
+var express = require("express");
 
-// Initialize the app and create a port
-const app = express();
-const PORT = process.env.PORT || 8080;
+// Express Setup
+var app = express();
+var PORT = process.env.PORT || 8080;
 
-// Set up body parsing, static, and route middleware
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.json());
+app.use(express.static('public'));
 
-// Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+// Routes
+require("./public/routes/apiRoutes")(app);
+require("./public/routes/htmlRoutes")(app);
+
+// Start server - Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});

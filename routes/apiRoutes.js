7004a8155d3dfa6d481df7530a5bbc4c1b07a838
Ayodeji Amosun <ayodeji.amosun04@gmail.com>
2020-08-12 13:36:26 -0500

    fixed the database location!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
   // API GET Request
   app.get("/api/notes", function(req, res) {
     // console.log("get");    
-    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/db.json"), "utf8"));
+    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"));
     res.send(notes);
     // console.log(notes);
   });
@@ -16,13 +16,13 @@ module.exports = function(app) {
   // API POST Request
   app.post("/api/notes", function(req, res) {
     // console.log("post");
-    let notesRead = JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/db.json"), "utf8"));
+    let notesRead = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"));
     let newNote = req.body;
     newNote.id = uuidv4();
     // console.log(newNote.id);
     notesRead.push(newNote);
     let notesWrite = JSON.stringify(notesRead);
-    fs.writeFileSync(path.join(__dirname, "../../db/db.json"), notesWrite, "utf8");
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), notesWrite, "utf8");
     // console.log(newNote);
     // console.log(notesRead);
     // console.log(notesWrite);
@@ -33,10 +33,10 @@ module.exports = function(app) {
   app.delete("/api/notes/:id", function(req, res) {
     // console.log("delete");
     // console.log(req.params.id);
-    let notesRead = JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/db.json"), "utf8"));
+    let notesRead = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"));
     let deleteId = req.params.id;
     let updatedNotes = JSON.stringify(notesRead.filter(el => el.id !== deleteId));
-    fs.writeFileSync(path.join(__dirname, "../../db/db.json"), updatedNotes, "utf8");
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), updatedNotes, "utf8");
     // console.log(updatedNotes);
     res.end();
   });
